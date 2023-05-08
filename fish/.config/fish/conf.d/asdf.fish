a70c0ae8091970677e4795384a78600ceacef1d0
Pascal Huber <pascal.huber@resolved.ch>
2023-05-08 20:38:52 +0200

    fish: unset some environment variables

--- fish/.config/fish/conf.d/asdf.fish
+++ fish/.config/fish/conf.d/asdf.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
 
+# TODO: check if file exists
 source ~/.asdf/asdf.fish
 source ~/.asdf/completions/asdf.fish
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ if status --is-login
     # XDG Environment Variables
     set -gx XDG_CACHE_HOME $HOME/.cache
     set -gx XDG_CONFIG_HOME $HOME/.config
+    # set -gx XDG_CONFIG_DIR /etc/xdg/:$XDG_CONFIGDIRS_
     set -gx XDG_DATA_HOME $HOME/.local/share
     set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
@@ -38,13 +39,23 @@ if status --is-login
         # TODO: check scaling chromium flag for sway
         # set -x CHROMIUM_FLAGS "--force-device-scale-factor=1"
         set -x CHROMIUM_FLAGS "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-        set -x QT_QPA_PLATFORM wayland
-        set -x QT_QPA_PLATFORMTHEME qt5ct
-        set -x QT_WAYLAND_FORCE_DPI physical
-        # set -x GDK_BACKEND wayland
-        set -x CLUTTER_BACKEND wayland
-        set -x WLR_DRM_NO_MODIFIERS 1
+
+        # TODO: Do I need these?
+        # set -x QT_QPA_PLATFORM wayland
+        # set -x QT_QPA_PLATFORMTHEME qt5ct
+        # set -x QT_WAYLAND_FORCE_DPI physical
+
+        # Don't do this:
+        # # set -x GDK_BACKEND wayland
+
+        # TODO: find out what they do
+        # set -x CLUTTER_BACKEND wayland
+        # set -x WLR_DRM_NO_MODIFIERS 1
+
+        # Run firefox with wayland
         set -x MOZ_ENABLE_WAYLAND 1
+
+        # Start sway with dbus session bus
         exec dbus-run-session sway
     end
 end

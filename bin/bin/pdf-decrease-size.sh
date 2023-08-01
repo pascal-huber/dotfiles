5a272bfb5022676c50c5375c15eb6f4865e5c74b
Pascal Huber <pascal.huber@resolved.ch>
2023-08-01 19:28:37 +0200

    update pdf-decrease-size.sh

--- bin/bin/pdf-decrease-size.sh
+++ bin/bin/pdf-decrease-size.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+#
+# Reduces the size of a pdf to a reasonable size
+#
+# Usage: pdf-decrease-size pdfs...
 
-files=$(find . -iname "*.pdf");
-for f in $files; do
+# options: /default, /prepress, /printer, /ebook, /screen
+QUALITY="/printer"
+
+for f in "$@"; do
     echo "$f"
     gs \
         -sDEVICE=pdfwrite \
         -dCompatibilityLevel=1.4 \
-        -dPDFSETTINGS=/prepress \
+        -dPDFSETTINGS=$QUALITY \
         -dNOPAUSE \
         -dQUIET \
         -dBATCH \

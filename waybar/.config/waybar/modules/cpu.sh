0e98a5954c9469549a597d7e05fd1804dc5cb054
Pascal Huber <pascal.huber@resolved.ch>
2026-08-16 21:48:47 +0200

    remove old waybar config

--- waybar/.config/waybar/modules/cpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-hwmon_dir="$(echo /sys/devices/pci0000:00/0000:00:18.3/hwmon/*)"
-temp_file="${hwmon_dir}/temp1_input"
-temp=$(cat "$temp_file" | awk '{print int($0/1000 + 0.5)}')
-
-echo "${temp}°C"
--- waybar/.config/waybar/modules/zfs_memory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-[ -f "/proc/spl/kstat/zfs/arcstats" ]
-
-awk '/^size/ { printf("%.1f GB\n", $3 / 1073741824) }' < /proc/spl/kstat/zfs/arcstats
